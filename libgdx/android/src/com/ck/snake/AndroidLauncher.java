c3d2ab8e8555d0ba139e92201980d98e783c95d8
knealy <knealy@splunk.com>
2016-03-13 17:45:37 -0700

    added source files for original libgdx project

---
+++ libgdx/android/src/com/ck/snake/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.ck.snake;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ck.snake.ProjectSnake;
+
+public class AndroidLauncher extends AndroidApplication {
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectSnake(), config);
+	}
+}
